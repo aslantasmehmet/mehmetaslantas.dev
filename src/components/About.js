e11b79c8ea51a15091b298d454561ea4e85ba7c1
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2023-01-25 12:12:20 +0300

    some bug fixed

--- src/components/About.js
+++ src/components/About.js
@@ -12,16 +12,12 @@ export default function About() {
       </div>
       <div className="col-span-2">
         <div className="flex flex-col">
-          <div className="mt-16  w-56 h-14 ml-96 relative overflow-hidden rounded-lg  px-20 py-6 ring-red-500/50 ring-offset-black will-change-transform focus:outline-none focus:ring-1 focus:ring-offset-2 ">
-            <span class="absolute inset-px z-10 grid place-items-center rounded-lg  bg-gradient-to-t from-neutral-800 text-white font-bold">
-              ABOUT ME
-            </span>
-            <span
-              aria-hidden
-              class="absolute inset-0 z-0 scale-x-[2.0] blur before:absolute before:inset-0 before:top-1/2 before:aspect-square before:animate-disco before:bg-gradient-conic before:from-purple-700 before:via-red-500 before:to-amber-400"
-            />
+        <div className="text-xl m-4 translate-y-5 sm:text-3xl  text-center text-black ">
+        <span className=" pl-2 select-none border-l-4 border-b-4 border-purple-500">
+          About Me
+        </span>
           </div>
-          <div className="mr-4 mt-8 font-semibold text-lg text-black border-b-4 border-orange-500 border-l-4 border-orange-500 ">
+          <div className="mr-4 mt-8 font-semibold text-lg text-black border-b-4 border-purple-500 border-l-4  ">
             I am a Software Developer who is committed to self-development and
             lifetime learning. I am always active and eager to learn new skills.
             I believe this way I can consistently achieve better results.
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -9,14 +9,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 export default function Skill() {
   return (
     <div className="bg-zinc-800 w-full h-96 -translate-y-2">
-      <div className="translate-y-16 translate-x-48 mt-16  w-56 h-14 ml-96 relative overflow-hidden rounded-lg  px-20 py-6 ring-red-500/50 ring-offset-black will-change-transform focus:outline-none focus:ring-1 focus:ring-offset-2 ">
-        <span class="absolute inset-px z-10 grid place-items-center rounded-lg  bg-gradient-to-t from-neutral-800 text-white font-bold">
-          SKİLL
+      <div className="text-xl m-4 translate-y-4 sm:text-3xl  text-center text-white ">
+        <span className=" pl-2 select-none border-l-4 border-b-4 border-purple-500">
+          Skills
         </span>
-        <span
-          aria-hidden
-          class="absolute inset-0 z-0 scale-x-[2.0] blur before:absolute before:inset-0 before:top-1/2 before:aspect-square before:animate-disco before:bg-gradient-conic before:from-purple-700 before:via-red-500 before:to-amber-400"
-        />
       </div>
       {/* slider */}
       <div className="container mx-auto translate-y-28">
@@ -32,38 +28,35 @@ export default function Skill() {
           onSlideChange={() => console.log("slide change")}
         >
           <SwiperSlide>
-            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" />
+            <img  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg" />
+            <img  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/react_k7sjy1.png" />
+            <img  src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/react_k7sjy1.png" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/next_m9wa8t.png" />
+            <img  src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/next_m9wa8t.png" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/tailwind_tprmt8.png" />
+            <img  src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/tailwind_tprmt8.png" />
           </SwiperSlide>
           <SwiperSlide>
             <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/bootstrap_fg0dzy.png" />
           </SwiperSlide>
 
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/css_nhk6k1.png" />
+            <img  src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/css_nhk6k1.png" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/html_lhoccu.png" />
+            <img  src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/html_lhoccu.png" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/git-logo_vqluql.png" />
+            <img  src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/git-logo_vqluql.png" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/github_eke0k2.png" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545775/bootstrap_fg0dzy.png" />
+            <img  src="https://res.cloudinary.com/dd4d48hwn/image/upload/v1674545774/github_eke0k2.png" />
           </SwiperSlide>
         </Swiper>
       </div>
