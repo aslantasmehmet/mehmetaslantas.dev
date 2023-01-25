8f5b71c8c393c72506f62307cb79098ba24ad852
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2023-01-25 17:12:01 +0300

    The router structure has been created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-
-import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import HeroSection from './components/HeroSection';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
-import Skill from './components/Skill';
+import { Route } from "react-router";
+import { Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skill from "./components/Skill";
+import { PortfolioProvider } from "./context/PortfolioContext";
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <HeroSection/>
-        <About/>
-        <Skill/>
-        <Projects/>
-        <Contact/>
-        <Footer/>
+      <PortfolioProvider>
+        <Routes>
+          <Route path="/*" element={<Navbar />} />
+          <Route path="" element={<HeroSection />} />
+          <Route path="About" element={<About />} />
+          <Route path="Skill" element={<Skill />} />
+          <Route path="Projects" element={<Projects />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="Footer" element={<Footer />} />
+        </Routes>
+      </PortfolioProvider>
     </div>
   );
 }
